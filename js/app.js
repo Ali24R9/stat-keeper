bdb55b58cfad2081c104bb8ca3b4ff629abb84e4
John Cech & Ali Abou-Zahr <student@epicodus.com>
2014-04-30 11:56:01 -0700

    add a player to a team but not save them to local storage

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
 App = Ember.Application.create();
 
 App.ApplicationSerializer = DS.LSSerializer.extend();
+
 App.ApplicationAdapter = DS.LSAdapter.extend({
     namespace: 'stat-keeper'
 });
 
 App.Router.map(function() {
   this.resource('teams', { path: '/'}, function() {
-    this.resource('team', { path: 'teams/:team_id' });
+    this.resource('team', { path: 'teams/:id' }, function() {
+      this.resource('new_player', { path: 'players/new' });
+      this.resource('players', {path: 'players'});
+    });
   });
 
+});
+
+App.NewPlayerRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.createRecord('player');
+  }
 });
 
 App.TeamsRoute = Ember.Route.extend({
@@ -21,7 +31,37 @@ App.TeamsRoute = Ember.Route.extend({
 App.TeamRoute = Ember.Route.extend({
   model: function(params) {
     var team = this.get('team');
-    return this.store.find('team', params.team_id);
+    return this.store.find('team', params.id);
+  }
+});
+
+App.PlayersRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('player')
+  }
+});
+
+App.NewPlayerController = Ember.ObjectController.extend({
+  needs: "team",
+  team: Ember.computed.alias("controllers.team"),
+  actions: {
+    createPlayer: function() {
+      var name = this.get('newName');
+      if (!name.trim()) {return;}
+
+
+      var team_id = this.get('controllers.team.id');
+
+      var player = this.store.createRecord('player', {
+        name: name
+      });
+
+      this.set('newName', '');
+
+      player.save();
+      var team = this.get('team').get('model');
+      team.get('players').pushObject(player);
+    }
   }
 });
 
@@ -42,7 +82,6 @@ App.TeamsController = Ember.ArrayController.extend({
       this.set('newName', '');
       this.set('newCity', '');
 
-
       team.save();
     }
   }
@@ -56,6 +95,6 @@ App.Team = DS.Model.extend({
 
 App.Player = DS.Model.extend({
   team: DS.belongsTo('team'),
-  name: DS.attr('string')
+  name: DS.attr('string'),
 });
 
